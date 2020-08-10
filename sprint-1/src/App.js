6c6be9fe8ce3767c76b5c636f8e795c45da1d67d
yashnagpaul <65575697+yashnagpaul@users.noreply.github.com>
2020-08-10 00:25:56 -0700

    cleaned up code, deleted comments, etc.

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -4,7 +4,7 @@ import MainVideo from "./components/MainVideo.jsx";
 import VideoList from "./components/VideoList/VideoList";
 import VideoDescription from "./components/VideoDescription";
 
-import "./styles/main.css";
+import "./styles/app.css";
 
 import { v4 as uuidv4 } from "uuid";
 
