fefc99957b090cc355b9089aadf607a5914a7b5f
yashnagpaul <65575697+yashnagpaul@users.noreply.github.com>
2020-08-18 20:05:29 -0700

    created client and server folders in sprint 3 + nodemon installed + 2 basic apiRoutes created

---
+++ sprint-3/server/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+
+app.use("/", apiRoutes);
+// app.use(app.apiRoutes);
+// apiRoutes.initialize(app);
+
+app.listen(8080, console.log("app running at http://localhost:8080"));
---
+++ sprint-3/server/routes/apiRoutes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+const listOfVideos = [
+  {
+    id: "1af0jruup5gu",
+    title: "BMX Rampage: 2018 Highlights",
+    channel: "Red Cow",
+    image: "https://i.imgur.com/l2Xfgpl.jpg",
+  },
+  {
+    id: "1ainjruutd1j",
+    title: "Become A Travel Pro In One Easy Lesson",
+    channel: "Todd Welch",
+    image: "https://i.imgur.com/5qyCZrD.jpg",
+  },
+  {
+    id: "1aivjruutn6a",
+    title: "Les Houches The Hidden Gem Of The Chamonix",
+    channel: "Cornelia Blair",
+    image: "https://i.imgur.com/yFS8EBr.jpg",
+  },
+  {
+    id: "1a3cjruucpf7",
+    title: "Travel Health Useful Medical Information For",
+    channel: "Glen Harper",
+    image: "https://i.imgur.com/MMDMgD7.jpg",
+  },
+  {
+    id: "1am3jruuwagz",
+    title: "Cheap Airline Tickets Great Ways To Save",
+    channel: "Emily Harper",
+    image: "https://i.imgur.com/ibLw5q5.jpg",
+  },
+  {
+    id: "1akljruuvhzt",
+    title: "Take A Romantic Break In A Boutique Hotel",
+    channel: "Ethan Owen",
+    image: "https://i.imgur.com/7rD6Mf6.jpg",
+  },
+  {
+    id: "1ae5jruuoc4q",
+    title: "Choose the Perfect Accommodations",
+    channel: "Lydia Perez",
+    image: "https://i.imgur.com/0hi3N4B.jpg",
+  },
+  {
+    id: "1a4kjruuedd9",
+    title: "Cruising Destination Ideas",
+    channel: "Timothy Austin",
+    image: "https://i.imgur.com/DDJNZNw.jpg",
+  },
+  {
+    id: "1a8qhruuzky3",
+    title: "Train Travel On Track For Safety",
+    channel: "Scotty Cranmer",
+    image: "https://i.imgur.com/i6S8m7I.jpg",
+  },
+];
+
+router.get("/", (req, res) => {
+  res.send("<h2>Welcome to Yash's API</h2><h4>Try the '/videos' path</h4>");
+});
+
+router.get("/videos", (req, res) => {
+  res.json(listOfVideos);
+});
+
+router.post("/videos", (req, res) => {
+  if (req.params.title && req.params.description) {
+    const newVid = { id: req.params.id, desc: req.params.description };
+    listOfVideos.push(newVid);
+  }
+  res.json({ status: "uploaded!" });
+});
+
+module.exports = router;
